b4df5ff3d158f8ccc8f282855c350acdf4ae6d43
Hongze Zhang <hongze.zhang@intel.com>
2022-12-08 15:43:06 +0800

    fixup

--- tools/gluten-te/examples/buildhere-veloxbe.sh
+++ tools/gluten-te/examples/buildhere-veloxbe.sh
@@ -4,10 +4,4 @@ set -ex
 
 BASEDIR=$(dirname $0)
 
-EXTRA_MAVEN_OPTIONS="-Pspark-3.2 \
-                     -Pbackends-velox 
-                     -DskipTests \
-                     -Dscalastyle.skip=true \
-                     -Dcheckstyle.skip=true"
-
-$BASEDIR/../cmvn.sh clean install $EXTRA_MAVEN_OPTIONS
+$BASEDIR/../cbash.sh tools/one_step_veloxbackend.sh
