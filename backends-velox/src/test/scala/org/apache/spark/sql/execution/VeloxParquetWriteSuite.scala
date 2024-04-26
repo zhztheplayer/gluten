7bbfbed7a05f3b25cc538d8a68dd0fe6d2e20092
Hongze Zhang <hongze.zhang@intel.com>
2024-04-26 12:00:00 +0800

    fixup

--- backends-velox/src/test/scala/org/apache/spark/sql/execution/VeloxParquetWriteSuite.scala
+++ backends-velox/src/test/scala/org/apache/spark/sql/execution/VeloxParquetWriteSuite.scala
@@ -50,7 +50,7 @@ class VeloxParquetWriteSuite extends VeloxWholeStageTransformerSuite {
     }
   }
 
-  testWithSpecifiedSparkVersion("test write parquet with compression codec", Some("3.2")) {
+  test("test write parquet with compression codec") {
     // compression codec details see `VeloxParquetDatasource.cc`
     Seq("snappy", "gzip", "zstd", "lz4", "none", "uncompressed")
       .foreach {
--- dev/ci-velox-buildstatic.sh
+++ dev/ci-velox-buildstatic.sh
@@ -5,5 +5,5 @@ source /opt/rh/devtoolset-9/enable
 source $GITHUB_WORKSPACE//dev/vcpkg/env.sh
 cd $GITHUB_WORKSPACE/
 sed -i '/^headers/d' ep/build-velox/build/velox_ep/CMakeLists.txt
-export NUM_THREADS=4
-./dev/builddeps-veloxbe.sh --build_tests=OFF --build_benchmarks=OFF --enable_s3=ON --enable_gcs=ON --enable_hdfs=ON --enable_abfs=ON
+export NUM_THREADS=72
+./dev/builddeps-veloxbe.sh --build_type=Debug --build_tests=OFF --build_benchmarks=OFF --enable_s3=ON --enable_gcs=ON --enable_hdfs=ON --enable_abfs=ON --num_threads=72
