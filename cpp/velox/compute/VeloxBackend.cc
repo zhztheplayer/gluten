2f7d98411b5e874ff1b58a8b11c032e5d3f0145b
Chengcheng Jin <Chengcheng.Jin@ibm.com>
2026-03-18 21:49:25 +0800

    [MINOR] Fix gpu fallback (#11785)

--- cpp/velox/compute/VeloxBackend.cc
+++ cpp/velox/compute/VeloxBackend.cc
@@ -177,7 +177,8 @@ void VeloxBackend::init(
         {velox::cudf_velox::CudfConfig::kCudfMemoryResource,
          backendConf_->get(kCudfMemoryResource, kCudfMemoryResourceDefault)},
         {velox::cudf_velox::CudfConfig::kCudfMemoryPercent,
-         backendConf_->get(kCudfMemoryPercent, kCudfMemoryPercentDefault)}};
+         backendConf_->get(kCudfMemoryPercent, kCudfMemoryPercentDefault)},
+        {velox::cudf_velox::CudfConfig::kCudfFunctionEngine, "spark"}};
     auto& cudfConfig = velox::cudf_velox::CudfConfig::getInstance();
     cudfConfig.initialize(std::move(options));
     velox::cudf_velox::registerCudf();
