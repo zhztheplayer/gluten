21a4c6cf6b8f6e03334a57c6fe17a13a0855f453
Hongze Zhang <hongze.zzz123@gmail.com>
2026-04-17 11:18:47 +0800

    style

--- gluten-substrait/src/main/scala/org/apache/gluten/expression/ExpressionConverter.scala
+++ gluten-substrait/src/main/scala/org/apache/gluten/expression/ExpressionConverter.scala
@@ -968,7 +968,8 @@ object ExpressionConverter extends SQLConfHelper with Logging {
         }
       })
     if (newOrdinal == -1) {
-      throw new IllegalStateException(s"Couldn't find $structField in ${input.attrs.mkString("[", ",", "]")}")
+      throw new IllegalStateException(
+        s"Couldn't find $structField in ${input.attrs.mkString("[", ",", "]")}")
     } else {
       BoundReference(newOrdinal, structField.dataType, structField.nullable)
     }
