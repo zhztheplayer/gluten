6700a9be24d4c2a8b69a9e03c55e488a4d85f033
Hongze Zhang <hongze.zzz123@gmail.com>
2026-07-03 12:47:48 +0200

    fixup

--- ep/build-velox/src/get-velox.sh
+++ ep/build-velox/src/get-velox.sh
@@ -150,6 +150,17 @@ function apply_compilation_fixes {
   git add ${VELOX_HOME}/CMake/resolve_dependency_modules/arrow/modify_arrow.patch # to avoid the file from being deleted by git clean -dffx :/
 }
 
+function apply_bloomfilter_final_patch {
+  echo "Applying bloom filter final-removal patch..."
+  pushd $VELOX_HOME
+  git apply --check ${CURRENT_DIR}/remove-bloomfilter-final.patch && \
+    git apply ${CURRENT_DIR}/remove-bloomfilter-final.patch || {
+    echo "Failed to apply bloom filter final-removal patch"
+    exit 1
+  }
+  popd
+}
+
 function setup_linux {
   local LINUX_DISTRIBUTION=$(. /etc/os-release && echo ${ID})
   local LINUX_VERSION_ID=$(. /etc/os-release && echo ${VERSION_ID})
@@ -231,5 +242,7 @@ fi
 apply_provided_velox_patch
 
 apply_compilation_fixes
+
+apply_bloomfilter_final_patch
 
 echo "Finished getting Velox code"
